d17a4543069f571a5c1117681c694d3e59a04e69
Mahesh <umamahesh690@gmail.com>
2024-06-13 22:28:12 +0530

    ec2 module created

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable ami {
+  type        = string
+  default     = ""
+  description = "ec2 instance ami id"
+}
+variable instance_type {
+  type        = string
+  default     = ""
+  description = "ec2 instance type"
+}
